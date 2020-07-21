73343df829b8ea94bf48cf23b44f3b6a4f14a020
J-Kyu <wjstkdrb91@naver.com>
2020-07-21 16:16:23 +0900

    화합 터치 #1
    player touch와 limit 부분 구현
    (선 위치 후 판단)

---
+++ Assets/Scripts/TouchObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchObject : MonoBehaviour
+{
+    private Vector3 MousePosition;
+
+    private Transform targetPos;
+
+    public Vector3 origMousePos{get;set;}
+    public Vector3 origTargetPos{get;set;}
+
+    public RectTransform boundaryTrans = null;
+
+    
+
+    void OnMouseDown(){
+
+        MousePosition = Input.mousePosition;
+        MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+
+        Debug.Log(MousePosition);
+
+        targetPos = this.gameObject.transform;
+
+        origMousePos = new Vector3(MousePosition.x,MousePosition.y,MousePosition.z);
+        origTargetPos = new Vector3(targetPos.position.x,targetPos.position.y,targetPos.position.z);       
+        
+    }
+
+    void OnMouseDrag(){
+
+        MousePosition = Input.mousePosition;
+        MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+
+        Vector3 movePos =  MousePosition - origMousePos;
+
+        //선 위치 후 판단
+        this.gameObject.transform.position = new Vector3(origTargetPos.x, origTargetPos.x+movePos.y,origTargetPos.z);   
+
+        if( this.gameObject.transform.localPosition.y < -boundaryTrans.rect.height/2 ){
+            this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, -boundaryTrans.rect.height/2,origTargetPos.z);   
+        }   
+        else if( this.gameObject.transform.localPosition.y > boundaryTrans.rect.height/2){
+            this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, boundaryTrans.rect.height/2,origTargetPos.z);   
+        }      
+                 
+    }
+    void OnMouseUp(){
+        if(targetPos !=  null){
+                targetPos.position = new Vector3(origTargetPos.x,origTargetPos.y,origTargetPos.z);
+            }
+            
+            targetPos = null;    
+    }
+}
