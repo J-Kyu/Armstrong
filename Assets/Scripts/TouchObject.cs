335556c6056447a5969eb58bb9b6d46780b0dda3
J-Kyu <wjstkdrb91@naver.com>
2020-07-26 16:36:32 +0900

    화합 터치 #1
    1. Multi Toutch
    * 이거 인식 안되면, collider를 설정한 상태에서 하나가 touch가 인식 되었다면, 다른 collider의 touch는 인식이 안되는 것이다.....즉, multi touch 인식은 collider가 아닌 다른 방법으로 해야할 것 같다...

--- Assets/Scripts/TouchObject.cs
+++ Assets/Scripts/TouchObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TouchObject : MonoBehaviour
 {
@@ -13,63 +14,79 @@ public class TouchObject : MonoBehaviour
 
     public RectTransform boundaryTrans = null;
 
+    private Color orgColor;
+
     private bool isSelected = false;    
 
-    // void OnMouseDown(){
 
-    //     MousePosition = Input.mousePosition;
-    //     MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+    void Start(){
+
+        orgColor = this.gameObject.GetComponent<Image>().color;
+    }
+
+    void OnMouseDown(){
+
+
+        this.gameObject.GetComponent<Image>().color = new Color(1,0,0);
+        // MousePosition = Input.mousePosition;
+        // MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
 
         
-    //     targetTrans = this.gameObject.transform;
+        // targetTrans = this.gameObject.transform;
 
-    //     origMousePos = new Vector3(MousePosition.x,MousePosition.y,MousePosition.z);
-    //     origTargetPos = new Vector3(targetTrans.position.x,targetTrans.position.y,targetTrans.position.z);       
+        // origMousePos = new Vector3(MousePosition.x,MousePosition.y,MousePosition.z);
+        // origTargetPos = new Vector3(targetTrans.position.x,targetTrans.position.y,targetTrans.position.z);       
        
-    //     isSelected = true;
-    // }
+        // isSelected = true;
+    }
 
     void OnMouseDrag(){
 
-        if(!isSelected){
-            MousePosition = Input.mousePosition;
-            MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+        // if(!isSelected){
+            
+        //     Debug.Log("New Touch: "+this.gameObject.transform.parent.name);
+
+        //     MousePosition = Input.mousePosition;
+        //     MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
 
             
-            targetTrans = this.gameObject.transform;
+        //     targetTrans = this.gameObject.transform;
 
-            origMousePos = new Vector3(MousePosition.x,MousePosition.y,MousePosition.z);
-            origTargetPos = new Vector3(targetTrans.position.x,targetTrans.position.y,targetTrans.position.z);       
+        //     origMousePos = new Vector3(MousePosition.x,MousePosition.y,MousePosition.z);
+        //     origTargetPos = new Vector3(targetTrans.position.x,targetTrans.position.y,targetTrans.position.z);       
         
-            isSelected = true;
+        //     isSelected = true;
             
-            return ;
-        }
+        //     return ;
+        // }
 
-        MousePosition = Input.mousePosition;
-        MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
+        // MousePosition = Input.mousePosition;
+        // MousePosition = Camera.main.ScreenToWorldPoint(MousePosition);
 
-        Vector3 movePos =  MousePosition - origMousePos;
+        // Vector3 movePos =  MousePosition - origMousePos;
 
  
-        //선 위치 후 판단
-        // this.gameObject.transform.position = new Vector3(origTargetPos.x, origTargetPos.y+movePos.y,origTargetPos.z);   
-        this.gameObject.transform.Translate( new Vector3(0, movePos.y *0.02f,0));   
+        // //선 위치 후 판단
+        // // this.gameObject.transform.position = new Vector3(origTargetPos.x, origTargetPos.y+movePos.y,origTargetPos.z);   
+        // this.gameObject.transform.Translate( new Vector3(0, movePos.y *0.02f,0));   
 
-        if( this.gameObject.transform.localPosition.y < -boundaryTrans.rect.height/2 ){
-            this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, -boundaryTrans.rect.height/2,origTargetPos.z);   
-        }   
-        else if( this.gameObject.transform.localPosition.y > boundaryTrans.rect.height/2){
-            this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, boundaryTrans.rect.height/2,origTargetPos.z);   
-        }    
+        // if( this.gameObject.transform.localPosition.y < -boundaryTrans.rect.height/2 ){
+        //     this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, -boundaryTrans.rect.height/2,origTargetPos.z);   
+        // }   
+        // else if( this.gameObject.transform.localPosition.y > boundaryTrans.rect.height/2){
+        //     this.gameObject.transform.localPosition = new Vector3(origTargetPos.x, boundaryTrans.rect.height/2,origTargetPos.z);   
+        // }    
                  
     }
      void OnMouseUp(){
+
+
+        this.gameObject.GetComponent<Image>().color = orgColor;
     //     if(targetTrans !=  null){
     //             targetTrans.position = new Vector3(origTargetPos.x,origTargetPos.y,origTargetPos.z);
     //         }
             
     //         targetTrans = null;    
-        isSelected = false;
+        // isSelected = false;
     }
 }
