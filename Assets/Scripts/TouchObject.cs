31b7d5bdcf75ee4d3001642d66a58c1f0e97c03c
J-Kyu <wjstkdrb91@naver.com>
2020-07-29 17:08:39 +0900

    화합 터치 #1 그림처럼 구현 (Dictionary로 touch.fingerId와 pos 수정)

--- Assets/Scripts/TouchObject.cs
+++ Assets/Scripts/TouchObject.cs
@@ -17,6 +17,7 @@ public class TouchObject : MonoBehaviour
 
     private Vector3 origMousePos;
     
+    private Dictionary<int,Vector2> touchDic = new Dictionary<int,Vector2>();
 
 
 
@@ -31,23 +32,36 @@ public class TouchObject : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position), Vector2.zero);
 
+            
 
             //first touch
             if (hit.collider != null && Input.GetTouch(i).phase == TouchPhase.Began)
             {
                 if(hit.collider.transform.tag == "Chair")
                 {
-                    orgColor = hit.collider.transform.GetComponent<Image>().color;
-                    hit.collider.transform.GetComponent<Image>().color  = new Color(1,0,0);
+                    //register fingerID and start Pos as pair
+                    touchDic.Add(Input.GetTouch(i).fingerId,Input.GetTouch(i).position);
                 }
             }
+
+            if (hit.collider != null && Input.GetTouch(i).phase == TouchPhase.Moved)
+            {
+                if(hit.collider.transform.tag == "Chair")
+                {
+                    //translate
+                    Vector2 movingPos = Input.GetTouch(i).position -  touchDic[Input.GetTouch(i).fingerId];
+                    hit.transform.Translate( new Vector3(0, movingPos.y *0.02f,0));
+                }
+            }
+
 
             //end touch
             if (hit.collider != null && Input.GetTouch(i).phase == TouchPhase.Ended)
             {
                 if(hit.collider.transform.tag == "Chair")
                 {
-                    hit.collider.transform.GetComponent<Image>().color  = orgColor;
+                    //remove fingerID and start Pos as pair
+                    touchDic.Remove(Input.GetTouch(i).fingerId);
                 }
             }
 
@@ -57,16 +71,4 @@ public class TouchObject : MonoBehaviour
 
 
     }    
-
-
-
-    /*
-    phone에서와 pc에서 test를 위해서 function만 고유하고 input 값을 유동적으로 받는다.
-    */
-    private void MoveTarget(Vector2 touch, GameObject target){
-        /*
-            move target on touch
-        */
-        target.transform.Translate( new Vector3(0, touch.y *0.02f,0));
-    }
 }
