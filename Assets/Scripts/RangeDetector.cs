c602dbaf3dff5123e08ad8017fa8e2c52f186ebe
J-Kyu <wjstkdrb91@naver.com>
2020-08-05 15:24:31 +0900

    Turn 구현 #6
    * 물살 구현

--- Assets/Scripts/RangeDetector.cs
+++ Assets/Scripts/RangeDetector.cs
@@ -91,8 +91,6 @@ public class RangeDetector : MonoBehaviour
 
     private void TurnHorizontal(float angle){
 
-        Debug.Log(bladeTrans.localEulerAngles.x + " | " + angle);
-
         if(bladeTrans.localEulerAngles.x > angle){
             bladeTrans.Rotate(new Vector3(-1,0,0) * turnSpeed * Time.deltaTime);
         }
---
+++ Assets/Scripts/WaveLine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveLine : MonoBehaviour
+{
+    
+    [SerializeField] List<Transform> waveTransformsList = null;
+
+    private float boatPower = 1.0f;
+    private float basicSpeed = 1.0f;
+    void Update(){
+        Waving();
+    }
+
+    private void Waving(){
+
+        basicSpeed = Mathf.Abs(Mathf.Sin(Time.time))*0.05f;
+
+        for(int i = 0; i < waveTransformsList.Count; i++){
+            if(waveTransformsList[i].localPosition.y  > -5120.0f){
+                
+                waveTransformsList[i].Translate( new Vector3(0, -basicSpeed*boatPower,0));   
+
+            }
+            else{
+                waveTransformsList[i].localPosition = new Vector3(0,0,0);
+            }
+        }
+
+        // Debug.Log(Mathf.Abs(Mathf.Sin(Time.time)));
+    }
+
+    public void IncreaseSpeed(){
+        boatPower += 0.5f;
+    }
+
+    public void DecreaseSpeed(){
+
+
+        boatPower -= 0.5f;
+
+        if(boatPower <= 0.0f){
+            boatPower = 1.0f;
+        }
+    }
+
+
+}
