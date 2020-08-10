f12cc3d28eaccdc066c9aa9b1a196c744cadddea
J-Kyu <wjstkdrb91@naver.com>
2020-08-10 15:03:16 +0900

    Rowing Test를 위한 Master Seat #7
    * TouchObject에서 tag로 함수를 call할 때, MasterChair은 MasterChairMovement라는 script가 따로 존재하여 문제가 발생함 -> 새로운 tag 생성하여 switch로 해결 (생략 부분 추가)

--- Assets/Scripts/TouchObject.cs
+++ Assets/Scripts/TouchObject.cs
@@ -51,15 +51,11 @@ public class TouchObject : MonoBehaviour
               
                         break;
                     }
+                    default:{
+                        LogContent.instance.SaveLog(this.gameObject.name, "Touch Start");
+                        break;
+                    }
                 }
-                // if(hit.collider.transform.tag == "Chair")
-                // {
-                //     //register collider chair
-                //     chairDic.Add(Input.GetTouch(i).fingerId, hit.transform.gameObject);
-                //     hit.transform.gameObject.GetComponent<ChairMovement>().ChairSelected();
-
-                //     LogContent.instance.SaveLog(this.gameObject.name, "Touch Start");
-                // }
             }
 
             if (hit.collider != null && Input.GetTouch(i).phase == TouchPhase.Moved)
@@ -76,14 +72,12 @@ public class TouchObject : MonoBehaviour
               
                         break;
                     }
+                    default:{
+                        LogContent.instance.SaveLog(this.gameObject.name, "Touch Start");
+                        break;
+                    }
+                    
                 }
-
-
-                // if(hit.collider.transform.tag == "Chair")
-                // {
-                //     //translate            
-                //     hit.transform.GetComponent<ChairMovement>().MoveChair(Input.GetTouch(i).position);
-                // }
             }
         }
     
@@ -102,7 +96,21 @@ public class TouchObject : MonoBehaviour
 
             if(!keyExist){
                 //remove
-                chairDic[key].GetComponent<ChairMovement>().ChairReleased();
+                switch(chairDic[key].transform.tag){
+                    case "Chair":{
+                        chairDic[key].GetComponent<ChairMovement>().ChairReleased();
+                        break;
+                    }
+                    case "MasterChair":{
+
+                        chairDic[key].GetComponent<MasterChairMovement>().ChairReleased();
+                        break;
+                    }
+                    default:{
+                        LogContent.instance.SaveLog(this.gameObject.name, "Touch Start");
+                        break;
+                    }
+                }        
                 chairDic.Remove(key);
             }
             keyExist = false;
